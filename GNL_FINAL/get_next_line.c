160e1b32eddccf82cc345959b938982c51038795
Jim MOUSSU <jmoussu@e1r11p17.42.fr>
2018-12-09 18:43:08 +0100

    malloc proteger fichier complet

--- GNL_FINAL/get_next_line.c
+++ GNL_FINAL/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jmoussu <jmoussu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/05 17:09:13 by jmoussu           #+#    #+#             */
-/*   Updated: 2018/12/08 21:20:26 by jmoussu          ###   ########.fr       */
+/*   Updated: 2018/12/09 18:37:37 by jmoussu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int			ft_error(int fd, char **line, int *size)
 	char	buf[BUFF_SIZE];
 
 	*size = 0;
-	if (fd < 0 || fd > 4864 || !line || read(fd, buf, 0) == -1)
+	if (fd < 0 || !line || read(fd, buf, 0) == -1)
 		return (1);
 	return (0);
 }
@@ -28,7 +28,8 @@ int			calcend(char **sfd, int i, int size, char **line)
 		i++;
 	if ((*sfd)[i] == '\0' && size < BUFF_SIZE && ft_strlen(*sfd) > 0)
 	{
-		*line = ft_strdupfree(*sfd);
+		if (!(*line = ft_strdupfree(*sfd)))
+			return (-1);
 		*sfd = *sfd + i;
 		return (1);
 	}
@@ -40,15 +41,18 @@ char		*lol(char *buff, char **sfd, int *i)
 	char *startsave;
 	char *line;
 
-	*sfd = ft_strjoinfree(*sfd, buff, 1);
+	if (!(*sfd = ft_strjoinfree(*sfd, buff, 1)))
+		return ((char *)(-1));
 	while (((*sfd)[*i] != '\n') && ((*sfd)[*i] != '\0'))
 		(*i)++;
 	if ((*sfd)[*i] == '\n')
 	{
 		(*sfd)[*i] = '\0';
-		line = ft_strdup(*sfd);
+		if (!(line = ft_strdup(*sfd)))
+			return ((char *)(-1));
 		startsave = *sfd;
-		*sfd = ft_strdup(*sfd + *i + 1);
+		if (!(*sfd = ft_strdup(*sfd + *i + 1)))
+			return ((char *)(-1));
 		free(startsave);
 		return (line);
 	}
@@ -58,28 +62,27 @@ char		*lol(char *buff, char **sfd, int *i)
 int			get_next_line(const int fd, char **line)
 {
 	static char *save[4864] = {NULL};
-	char		buff[BUFF_SIZE + 1];
-	int			size;
-	int			i;
-	char		*ptr;
+	t_var		var;
 
-	i = 0;
-	if (ft_error(fd, line, &size) == 1)
+	if ((!(var.i = 0)) && ft_error(fd, line, &(var.size)) == 1)
 		return (-1);
 	(save[fd]) ? (save[fd]) : (save[fd] = ft_strdup(""));
 	while (1)
 	{
 		if ((ft_strchr(save[fd], '\n')) == NULL)
 		{
-			size = read(fd, buff, BUFF_SIZE);
-			if (size == 0)
+			if ((var.size = read(fd, var.buff, BUFF_SIZE)) == 0)
 				break ;
-			if (size < 0)
+			else if (var.size < 0)
 				return (-1);
 		}
-		buff[size] = '\0';
-		if ((ptr = lol(buff, save + fd, &i)))
-			return ((*line = ptr) ? (1) : (0));
+		var.buff[var.size] = '\0';
+		if ((var.ptr = lol(var.buff, save + fd, &(var.i))) == NULL)
+			continue ;
+		if (var.ptr == (char *)(-1))
+			return (-1);
+		else
+			return ((*line = var.ptr) ? (1) : (1));
 	}
-	return (calcend(save + fd, i, size, line));
+	return (calcend(save + fd, var.i, var.size, line));
 }
--- GNL_FINAL/get_next_line.h
+++ GNL_FINAL/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: jmoussu <jmoussu@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/25 16:03:41 by jmoussu           #+#    #+#             */
-/*   Updated: 2018/12/09 15:25:02 by jmoussu          ###   ########.fr       */
+/*   Updated: 2018/12/09 18:38:09 by jmoussu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,16 @@
 # define GET_NEXT_LINE_H
 
 # include "libft/libft.h"
-# define BUFF_SIZE 9
+# define BUFF_SIZE 42
 
-int		get_next_line(const int fd, char **line);
+typedef struct		s_var
+{
+	char			buff[BUFF_SIZE + 1];
+	int				size;
+	int				i;
+	char			*ptr;
+}					t_var;
+
+int					get_next_line(const int fd, char **line);
 
 #endif
